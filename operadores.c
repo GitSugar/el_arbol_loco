2ad487df97d3f39fbd5277c79db4630b6bb027db
Pablo Bernal <49212610+randomtextdude@users.noreply.github.com>
2021-05-29 19:27:35 -0300

    Create operadores.c
    viste cuando no sabes hacer push? bueno, asi

---
+++ operadores.c
@@ -0,0 +1,67 @@
+#include <math.h>
+#include "operadores.h"
+
+
+
+
+
+int suma(int *args){
+    int res;
+    res = args[0] + args[1];
+    return res;
+}
+
+
+
+int resta(int *args){
+    int res;
+    res = args[0] - args[1];
+    return res;
+}
+
+
+
+int opuesto(int *args){
+    int op;
+    op = args[0] * (-1);
+    return op;
+}
+
+
+
+int producto(int *args){
+    int res;
+    res = args[0] * args[1];
+    return res;
+}
+
+
+
+int division(int *args){
+    int res;
+    if (args[1] != 0){
+        res = args[0] / args[1];
+        return args;
+    }
+}
+
+
+
+int modulo(int *args){
+    int res;
+    if (args[0] < 0){
+        res = opuesto(args);
+        return res;
+    }
+    return args[0];
+}
+
+
+int potencia(int *args){
+    int res;
+    res = pow(args[0],args[1]);
+    return res;
+}
+
+
+
